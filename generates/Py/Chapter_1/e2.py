5f40c6f39054c65952145b782a91bdaf1703e2f4
williebonavente <76456752+williebonavente@users.noreply.github.com>
2023-08-03 23:01:46 +0800

    Changes in 3 part - Binary Linear and Bubble sort.

--- generates/Py/Chapter_1/e2.py
+++ generates/Py/Chapter_1/e2.py
@@ -12,3 +12,23 @@ def binary_search(arr, target):
             high = mid - 1
 
     return -1
+
+def is_valid_pin(pin, valid_pins):
+    index = binary_search(valid_pins, pin)
+    return index != -1
+
+def main():
+    # List of valid PINs
+    valid_pins = [1234, 5678, 9876, 4321, 5555]
+
+    # Get the PIN from the user
+    pin = int(input("Enter your PIN: "))
+
+    # Check if the PIN is valid
+    if is_valid_pin(pin, valid_pins):
+        print("Access granted. Welcome!")
+    else:
+        print("Invalid PIN. Access denied.")
+
+if __name__ == "__main__":
+    main()
