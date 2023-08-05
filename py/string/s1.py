33c473caefe0c94d42381166c6fb6e7ef972c275
williebonavente <76456752+williebonavente@users.noreply.github.com>
2023-08-05 22:14:01 +0800

    Some String Functions

---
+++ py/string/s1.py
@@ -0,0 +1,5 @@
+def reverse_string(string):
+    return string[::-1]
+
+string = "Hello Willie"
+print(reverse_string(string))
---
+++ py/string/s3.py
@@ -0,0 +1,2 @@
+def are_anagrams(string1, string2):
+    return sorted(string1) == sorted(string2)
