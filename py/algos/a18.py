5941d5f80ecd3234102f1679ccd87de697ea513f
williebonavente <76456752+williebonavente@users.noreply.github.com>
2023-08-06 23:40:34 +0800

    Trying to be organize.

--- py/algos/a18.py
+++ py/algos/a18.py
@@ -6,9 +6,61 @@ from Crypto.Cipher import DES
 from Crypto.Random import get_random_bytes
 
 def encrypt_des(key, data):
+    """
+    Encrypts the provided data using the Data Encryption Standard (DES) algorithm.
+
+    Args:
+        key (bytes): The 8-byte encryption key.
+        data (bytes): The data to be encrypted.
+
+    Returns:
+        bytes: The encrypted data.
+
+    Raises:
+        ValueError: If the key length is not 8 bytes.
+    """
+    if len(key) != 8:
+        raise ValueError("Key must be 8 bytes in length")
+
     cipher = DES.new(key, DES.MODE_ECB)
     return cipher.encrypt(data)
 
 def decrypt_des(key, encrypted_data):
+    """
+    Decrypts the provided encrypted data using the Data Encryption Standard (DES) algorithm.
+
+    Args:
+        key (bytes): The 8-byte decryption key.
+        encrypted_data (bytes): The encrypted data to be decrypted.
+
+    Returns:
+        bytes: The decrypted data.
+
+    Raises:
+        ValueError: If the key length is not 8 bytes.
+    """
+    if len(key) != 8:
+        raise ValueError("Key must be 8 bytes in length")
+
     cipher = DES.new(key, DES.MODE_ECB)
     return cipher.decrypt(encrypted_data)
+
+# Example usage of the DES encryption and decryption functions
+
+# Generate a random 8-byte key
+key = get_random_bytes(8)
+
+# Data to be encrypted
+data_to_encrypt = b"Hello, this is a secret message."
+
+# Encrypt the data
+encrypted_data = encrypt_des(key, data_to_encrypt)
+
+# Print the encrypted data
+print("\nEncrypted Data:", encrypted_data)
+
+# Decrypt the data
+decrypted_data = decrypt_des(key, encrypted_data)
+
+# Print the decrypted data
+print("\nDecrypted Data:", decrypted_data)
--- py/algos/a21.py
+++ py/algos/a21.py
@@ -7,4 +7,27 @@
 from sympy import factorint
 
 def factor_integer(n):
+    """
+    Factorizes an integer into its prime factors using the sympy library.
+
+    Args:
+        n (int): The integer to be factorized.
+
+    Returns:
+        dict: A dictionary containing the prime factors of the integer and their respective powers.
+
+    Raises:
+        ValueError: If the input is not a positive integer.
+    """
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("Input must be a positive integer.")
+
     return factorint(n)
+
+try:
+    num = int(input("Enter a positive integer: "))
+    result = factor_integer(num)
+    print(f"The prime factors of {num} are: {result}")
+except ValueError as e:
+    print("Error:", e)
+
