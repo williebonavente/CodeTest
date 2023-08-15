b1a48afbe6a2abd167b30a00d66b84b100cb04b0
Willie M. Bonavente <76456752+williebonavente@users.noreply.github.com>
2023-08-15 23:20:47 +0800

    Revert "Abstract Data Type Added"

--- notebooks/create.py
+++ notebooks/create.py
@@ -1,26 +1,8 @@
-class Stack:
-    def __init__(self):
-        self.items = []
+"""
+    creating helper function
+"""
 
-    def is_empty(self):
-        return self.items == []
-
-    def push(self, item):
-        self.items.append(item)
-
-    def pop(self):
-        return self.items.pop()
-
-    def peek(self):
-        return self.items[len(self.items)-1]
-
-    def size(self):
-        return len(self.items)
-
-def main():
-    s = Stack()
-    s.push(1)
-    print(s.peek())
+def hello_world():
+    print("Hello World")
     
-if __name__ == "__main__":
-    main()
+hello_world()
