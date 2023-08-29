1cd0f03777b0a698e25100ba23873a463687978c
Willie M. Bonavente <76456752+williebonavente@users.noreply.github.com>
2023-08-29 18:27:08 +0800

    Declare Variables

---
+++ golang/hello1.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+
+func main1() {
+	var student1 string = "John"
+	var student2 string = "Jane"
+	x := 2
+
+	fmt.Println(student1)
+	fmt.Println(student2)
+	fmt.Println(x)
+
+}
