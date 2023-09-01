b8ac0a94289ed3667883a778116f900b4e78e806
Willie M. Bonavente <76456752+williebonavente@users.noreply.github.com>
2023-09-01 23:29:30 +0800

    Append Functions with return values

--- golang/funcs.go
+++ golang/funcs.go
@@ -15,3 +15,13 @@ func main36(fname string) {
 func main37(fname string, age int) {
 	fmt.Println("Hello", fname, "Bonavente\n", "You are", age, "years young.")
 }
+
+// Returning
+func main38(x int, y int) int {
+	return x + y
+}
+
+func main39(x int, y int) (result int ) {
+	result = x + y 
+	return 
+}
