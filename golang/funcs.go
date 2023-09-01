5eec13300ca4eb0eaa79b35668b7a185938c6b11
Willie M. Bonavente <76456752+williebonavente@users.noreply.github.com>
2023-09-01 23:40:24 +0800

    Appending and Completing Function

--- golang/funcs.go
+++ golang/funcs.go
@@ -25,3 +25,28 @@ func main39(x int, y int) (result int ) {
 	result = x + y 
 	return 
 }
+
+func main40(x int, y string) (result int, txt1 string) {
+	result = x + x
+	txt1 = "Hi" + y 
+	return 
+}
+
+//..Recursive 
+func main41_recur(x int ) int {
+	if x == 11 {
+		return 0
+	}
+	fmt.Println(x)
+	return main41_recur(x + 1)
+}
+
+//..Factorial recursive 
+func main42_factorialRecursive(x float64) (y float64) {
+	if x > 0 {
+		y = x * main42_factorialRecursive(x - 1)
+	} else {
+		y = 1
+	}
+	return 
+}
