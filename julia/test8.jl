292742759dbd0a90bb7f91827f96df11969c223b
Willie M. Bonavente <76456752+williebonavente@users.noreply.github.com>
2023-09-19 23:31:44 +0800

    Quadratic function - julia

---
+++ julia/test8.jl
@@ -0,0 +1,13 @@
+# calculates x for 0 = a*x^2+b*x+c, arguments types can be defined in function definitions
+function quadratic2(a::Float64, b::Float64, c::Float64)
+    # unlike other languages 2a is equivalent to 2*a
+    # a^2 is used instead of a**2 or pow(a,2)
+    sqr_term = sqrt(b^2-4a*c)
+    r1 = quadratic(a, sqr_term, b)
+    r2 = quadratic(a, -sqr_term, b)
+    # multiple values can be returned from a function using tuples
+    # if the return keyword is omitted, the last term is returned
+    r1, r2
+end
+
+quadratic2(1.4, 4.2, -4.3)
