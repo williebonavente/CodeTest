c892c089664c4669032ef09c09bd7589fc301ace
Willie M. Bonavente <76456752+williebonavente@users.noreply.github.com>
2023-09-19 23:34:04 +0800

    Rendering alpha, beta and gamma.

---
+++ julia/test9.jl
@@ -0,0 +1,5 @@
+# strings
+# strings are defined with double quotes
+# like variables, strings can contain any unicode character
+s1 = "The quick brown fox jumps over the lazy dog α,β,γ"
+println(s1)
