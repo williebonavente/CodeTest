7012725edc718ca54ea3da48985b204411c4dede
Willie M. Bonavente <76456752+williebonavente@users.noreply.github.com>
2023-10-09 23:45:14 +0800

    Add MergeSort

---
+++ cantSolve/MergeSort.py
@@ -0,0 +1,9 @@
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+    left = merge_sort(array[:mid])
+    right = merge_sort(array[mid:])
+
+    return merge_sort(left, right)
